474b0240fbaf1276193e686692ae31daac4a7944
raviOO7 <ravikhatrithedon@gmail.com>
2019-10-03 16:35:57 +0530

    Converted blueprint class to cpp

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -3,12 +3,27 @@
 
 #include "ChooseNextWaypoint.h"
 #include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+#include "PatrollingGuard.h"    // TODO removing coupling
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
+    // TODO protect against empty patrol routes
+
+    // Get the patrol points
+    auto AIController = OwnerComp.GetAIOwner();
+    auto ControlledPawn = AIController->GetPawn();
+    auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
+    auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+
+    // Set next waypoint
     auto BlackboardComp = OwnerComp.GetBlackboardComponent();
     auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+    BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
 
-    UE_LOG(LogTemp, Warning, TEXT("Waypoint index: %i"), Index)
+    // Cycle the index
+    auto NextIndex = (Index + 1) % PatrolPoints.Num();
+    BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+    
     return EBTNodeResult::Succeeded;
 }
--- Source/TestingGrounds/ChooseNextWaypoint.h
+++ Source/TestingGrounds/ChooseNextWaypoint.h
@@ -20,4 +20,7 @@ class TESTINGGROUNDS_API UChooseNextWaypoint : public UBTTaskNode
 	UPROPERTY(EditAnywhere, Category = "Blackboard")
 	struct FBlackboardKeySelector IndexKey;
 
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector WaypointKey;
+
 };
